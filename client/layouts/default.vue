9bcd6d55a41e97aef5214a87cff5cfb125e7b714
Cerberrus <genochka_kravchenko@mail.ru>
2020-10-07 02:05:35 +0300

    init + authorization page + collection page without modals

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,19 +1,193 @@
 <template>
-	<div>
-		<nuxt/>
-	</div>
+  <div class="page__body container">
+    <nuxt/>
+    <footer class="footer">
+      <p>Powered by <a href="" target="_blank" rel="noopener">MegaVasily007</a>, <a href="http://aurora-webstudio.com/" target="_blank" rel="noopener">Aurora Web</a></p>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {
-	name: 'default',
-	mounted() {
-		if (localStorage.name)
-			this.$store.commit('auth', {username: localStorage.name, hash: localStorage.hash});
-	}
-};
+  export default {
+    name: 'default',
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+    mounted() {
+      if (localStorage.name)
+        this.$store.commit('auth', {username: localStorage.name, hash: localStorage.hash});
+    }
+  };
 </script>
 
-<style scoped>
+// Global styles
+<style>
+:root {
+  /* Colors */
+  --color-gray1: #333;
 
+  /* Fonts */
+  --equestria: 'Equestria', serif;
+  --roboto:    'Roboto', 'Arial', sans-serif;
+
+  /* Other */
+  --container-width: 340px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  line-height: 1.32;
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+
+.page__body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  font: normal 16px/132% var(--roboto);
+  color: #fff;
+  word-break: break-word;
+  overflow-x: hidden;
+  background: linear-gradient(168.75deg, #F6F3B0 10.76%, #E99557 65.82%, #E15D51 95.82%);
+}
+
+main {
+  margin: auto;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  color: #fff;
+  border-bottom: 1px solid #fff;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #7C29A3;
+  border-color: #7C29A3;
+}
+
+h1 {
+  font-family: var(--equestria);
+  font-size: 48px;
+  font-weight: 400;
+}
+
+button,
+.button {
+  display: inline-block;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 17px 0;
+  text-align: center;
+  font-family: var(--equestria);
+  font-size: 20px;
+  color: #fff;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  border-radius: 50px;
+  background: linear-gradient(91.88deg, rgba(124, 41, 163, 0.7) -11.44%, rgba(212, 0, 217, 0.7) 106.2%);
+  box-shadow: 2px 4px 6px rgba(221, 34, 214, 0.25);
+}
+
+svg {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.icon {
+  fill: var(--color-gray1);
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  object-fit: cover;
+  object-position: center;
+}
+
+.container{
+  padding-left:  calc(50% - var(--container-width) / 2) !important;
+  padding-right: calc(50% - var(--container-width) / 2) !important;
+}
+
+.srOnly {
+  border: 0;
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+  word-wrap: normal !important;
+}
+
+.fixed {
+  overflow: hidden;
+}
+
+/* Forms */
+.form__input {
+  width: 100%;
+  padding: 12px 16px;
+  font-size: 16px;
+  color: #fff;
+  border: none;
+  border-radius: 50px;
+  background: linear-gradient(91.88deg, rgba(124, 41, 163, 0.4) -11.44%, rgba(212, 0, 217, 0.4) 106.2%);
+  box-shadow: inset 2px 4px 6px rgba(0, 0, 0, 0.25);
+}
+
+.form__input::placeholder {
+  padding-bottom: 4px;
+  border-bottom: 1px solid #fff;
+  color: #fff;
+}
+
+.form__input:focus {
+  outline: none;
+}
+
+
+.footer {
+  margin: 16px 0;
+}
+</style>
+
+// Fonts
+<style>
+/* Equestria-regular */
+@font-face {
+  font-family: 'Equestria';
+  font-style: normal;
+  font-weight: 400;
+  src: local(''),
+  url('~static/fonts/Equestria.woff2') format('woff2'),
+  url('~static/fonts/Equestria.woff') format('woff');
+}
+
+/* Roboto-regular */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Roboto'), local('Roboto-Regular'),
+  url('~static/fonts/roboto-v20-latin_cyrillic-regular.woff2') format('woff2'),
+  url('~static/fonts/roboto-v20-latin_cyrillic-regular.woff') format('woff');
+}
 </style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,25 +1,43 @@
 <template>
-	<div>
-		<h1>Main page</h1>
-		<div v-if="state === 0">
-			<input v-model="username" type="text" placeholder="Имя">
-			<button @click="login">Полетели</button>
-		</div>
-		<div v-if="state === 1">
-			<p>{{ username }}</p>
-			<button @click="scan">Сканировать</button>
-			<div>
-				<p>Команда:</p>
-				<div v-for="i in tasks.all">
-					<span v-if="tasks.complete.indexOf(i) > -1">+</span>
-					<span v-if="tasks.failed.indexOf(i) > -1">-</span>
-				</div>
-			</div>
-		</div>
-		<div v-if="state === 2">
-			<qrcode-capture @decode="onDecode"></qrcode-capture>
-		</div>
-	</div>
+  <main>
+      <!--  Auth  -->
+      <div v-if="state === 0" class="authorization">
+        <h1 class="authorization__title">Собери команду</h1>
+        <form class="authorization__form form">
+          <label class="form__label">
+            <span class="srOnly">Введите ваше имя</span>
+            <input v-model="username" type="text" placeholder="Имя" class="form__input">
+          </label>
+          <button @click.prevent="login" class="authorization__button">Полетели</button>
+        </form>
+      </div>
+
+      <!--  Scan  -->
+      <div v-if="state === 1" class="collection">
+        <h1 class="srOnly">Ваш профиль</h1>
+        <div class="collection__head">
+          <p class="collection__name">{{ username }}</p>
+          <img src="/images/collectBg.png" alt="">
+        </div>
+        <button @click="scan" class="collection__scanButton">Сканировать код</button>
+        <button @click="" class="collection__helpButton">Помощь</button>
+
+        <section class="collection__team team">
+          <h2 class="team__title">Ваша команда</h2>
+          <ul class="team__list">
+            <li v-for="i in tasks.all" class="team__item">
+              <img v-if="tasks.complete.indexOf(i) > -1" :src="`/images/${i}.jpg`" alt=""/>
+              <img v-if="tasks.failed.indexOf(i) > -1" src="/images/inaccessible.jpg" alt=""/>
+            </li>
+          </ul>
+        </section>
+      </div>
+
+      <!--  Qrcode  -->
+      <div v-if="state === 2">
+        <qrcode-capture @decode="onDecode"></qrcode-capture>
+      </div>
+  </main>
 </template>
 
 <script>
@@ -36,7 +54,7 @@ export default {
 		...mapGetters(['isAuth', 'getName'])
 	},
 	async asyncData({ $axios }) {
-		let tasks;
+		let tasks = {};
 		if (localStorage.name)
 			tasks = await $axios.$get('/sync');
 
@@ -74,5 +92,75 @@ export default {
 </script>
 
 <style scoped>
+  .authorization {
+    padding-top: calc(10% + 294px + 4%);
+    padding-bottom: 100px;
+    background-image: url('/images/authBg.png');
+    background-size: 100% 294px;
+    background-position: center 10%;
+    background-repeat: no-repeat;
+  }
 
+  .authorization__title {
+    margin-bottom: 48px;
+  }
+
+  .authorization__button {
+    margin-top: 16px;
+  }
+
+  /* Collection */
+  .collection__head {
+    display: grid;
+    justify-items: center;
+  }
+
+  .collection__name {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    padding-top: 24px;
+    padding-bottom: 32px;
+    font-family: var(--equestria);
+    font-size: 32px;
+    text-align: center;
+    background: linear-gradient(180deg, rgba(242, 153, 74, 0.5) 0%, rgba(242, 153, 74, 0) 100%);
+  }
+
+  .collection__scanButton {
+    margin-top: 24px;
+    box-shadow: 2px 4px 6px rgba(221, 34, 214, 0.25);
+  }
+
+  .collection__helpButton {
+    margin-top: 8px;
+    padding: 0;
+    background: none;
+    box-shadow: none;
+  }
+
+  .collection__team {
+    margin: 32px 0;
+  }
+
+  .team__title {
+    margin-bottom: 16px;
+    font-family: var(--equestria);
+    font-size: 26px;
+    font-weight: 400;
+  }
+
+  .team__list {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 10px;
+    flex-wrap: wrap;
+  }
+
+  .team__item {
+    display: flex;
+    border-radius: 50%;
+    overflow: hidden;
+  }
 </style>
